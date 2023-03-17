50a26f3ff73c59213b7a404118256f874141d4b8
fabriciocavalcante27 <fabriscavalcante@gmail.com>
2023-03-16 22:13:03 -0300

    Esse é o meu quarto commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocarimagem(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+}
